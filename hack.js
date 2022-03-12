82beb1da4f7ae69d6e437323767f4a0f6a55c27d
Chris Eaton <36697853+CEatonTX@users.noreply.github.com>
2022-03-12 12:12:12 -0600

    If no root access, try to get it using getroot.js

--- hack.js
+++ hack.js
@@ -8,11 +8,12 @@ run hack.js target -t (num of threads)
 /** @param {NS} ns **/
 export async function main(ns) {
 	var target = ns.args[0];
-	ns.tprint("Hacking sever " + target);
+	ns.tprint("Hacking server " + target);
 	var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
 	var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+	var loop = true;
 
-	while (true) {
+	while (loop == true) {
 		if (ns.hasRootAccess(target)) {
 			if (ns.getServerSecurityLevel(target) > securityThresh) {
 				await ns.weaken(target);
@@ -21,6 +22,11 @@ export async function main(ns) {
 			} else {
 				await ns.hack(target);
 			}
+		}
+		else {
+			ns.tprint("Cannot hack server, attempting to get root access on " + target);
+			ns.run("getroot.js", 1, target);
+			await ns.sleep(60000); // Wait on minute and try again. 
 		}
 	}
 }
