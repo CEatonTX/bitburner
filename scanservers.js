9f9b99797154b454c0c09d173cb4da61468b5879
Chris Eaton <36697853+CEatonTX@users.noreply.github.com>
2022-03-30 14:07:04 -0500

    Update scanservers.js

--- scanservers.js
+++ scanservers.js
@@ -18,10 +18,21 @@ export async function main(ns) {
     }
 
     function addtoServers(servername) {
+        let currentServerNamesList = serverarray.map(s => s.name);
+        let isItANewServer = true;
+        for (let i = 0; i < currentServerNamesList.length; i++) {
+            //ns.tprint("DEBUG=> currentServerNamesList[" + i + "] = " +currentServerNamesList[i]  );
+            if (currentServerNamesList[i] == servername) { isItANewServer = false }
+        }
         //if the server does not exsist add it
-        if (serverarray.find(server => server.name === servername) !== null) {
+        if (isItANewServer) {
+            //ns.tprint("DEBUG=> I think that server " + servername + " does not exsist.");
             //unless if the server name starts with pserv, don't add those!
-            if (servername.substring(0, 5) !== "pserv") {
+            if (servername.startsWith("pserv")) {
+                //do nothing. 
+            }
+            else {
+                //ns.tprint("Adding new server: " + servername);
                 serverarray.push(new Servers(
                     servername,  // name of server. duh. 
                     'notusedyet',      // one step prior connection
@@ -30,6 +41,7 @@ export async function main(ns) {
                     ns.getServerNumPortsRequired(servername),           // numPortsRequired
                     ns.getServerMaxRam(servername)            // maxRam
                 ))
+
             }
         }
         else {
@@ -37,33 +49,70 @@ export async function main(ns) {
         }
     }
 
-    let serverarray = []
-    // initial scan of root to get base level servers
-    try {
-        let currentServers = ns.scan("home");
+    function scanAndAddNewServers(value) {
+        let currentServers = {};
+        //ns.tprint("DEBUG: scanning: " + value);
+        // make sure that a server name has been passed to the function to scan
+        if (value !== "home") {
+            // ns.tprint("server specified as:" + value);
+            currentServers = ns.scan(value);
+        }
+        // if no server was specified then scan home
+        else {
+            // ns.tprint("server name is home")
+            currentServers = ns.scan();
+        }
+
         currentServers.forEach(
             servername => addtoServers(servername)
         );
     }
-    catch (error) {
-        ns.tprint("ERROR! " + error);
+
+    function updateServerList() {
+        // this will scan the current list of servers 
+        let newServerFound = true;
+        let firstServerCount = 0;
+        let secondServerCount = 0;
+        while (newServerFound) {
+            let currentServerNamesList = serverarray.map(s => s.name);
+            firstServerCount = currentServerNamesList.length;
+            //ns.tprint("DEBUG firstserverCount=" + firstServerCount);
+            currentServerNamesList.forEach(name => scanAndAddNewServers(name));
+            currentServerNamesList = serverarray.map(s => s.name);
+            secondServerCount = currentServerNamesList.length;
+            //ns.tprint("DEBUG firstserverCount=" + secondServerCount);
+            if (secondServerCount == firstServerCount) {
+                newServerFound = false
+            }
+        }
     }
 
+    let serverarray = []  // This will store all our data using the schema defined above. 
+    // initial scan of root to get base level servers
+    scanAndAddNewServers("home");
+    // update the server list by scanning everything known until there are no new knowns:
+    updateServerList();
 
+    // ##### DEBUG Uncomment below to print all known server names
+    ns.tprint(serverarray.map(s => s.name));
 
-
-
-
-    // for debug print all data uncomment below:
+    // ##### DEBUG Uncomment below to print all data in the array
     // serverarray.forEach(value => ns.tprint(value));
+ 
+
+
+
+    //ns.tprint("DEBUG:=> serverarray.length=" + serverarray.length);
+    // for debug print all data uncomment below:
+    // serverarray.forEach(value => ns.tprint(value.name));
 
     // example of how to pull data from the array: 
-    ns.tprint(
-        "DEBUG: " +
-        serverarray.find(server => server.name === "n00dles").maxRam
-    );
+    //ns.tprint(
+    //    "DEBUG: " +
+    //    serverarray.find(server => server.name === "n00dles").maxRam
+    //);
 
-    ns.tprint(ns.getServerMaxRam("n00dles"));
+    //ns.tprint(ns.getServerMaxRam("n00dles"));
 
     // but this does not work :( Why? 
     //ns.tprint(serverarray.n00dles.rootAccess);
